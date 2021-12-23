77ad85c856f6dd2c9aea212227f7f0814d217854
Omar Abuhadid <omrs2002@yahoo.com>
2021-12-23 16:49:08 +0300

    Add project files.

---
+++ MVCWizard.Api/Data/EmployeeDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCWizard.Api.Data.Models;
+
+namespace MVCWizard.Api.Data
+{
+    public class EmployeeDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            optionsBuilder.UseSqlServer(connStr);
+            base.OnConfiguring(optionsBuilder);
+            
+        }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options) : base(options)
+        {
+            
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+
+
+    }
+}
---
+++ MVCWizard.Api/Data/Models/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCWizard.Api.Data.Models
+{
+    public class Employee
+    {
+        
+        [Key]
+        public int Id { get; set; }
+        
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public DateOnly DateOfBirth { get; set; }
+        
+        [Required]
+        public string Gender { get; set; }
+
+
+        [Required]
+        public string Dept { get; set; }
+
+        [Required]
+        public double   Salary { get; set; }
+
+
+        [Required]
+        public DateOnly DateOfStart { get; set; }
+
+
+        [Required]
+        public string Bio { get; set; }
+
+
+
+        [Required]
+        public bool CompletionStatus { get; set; } = false;
+
+
+
+
+
+
+
+    }
+}
