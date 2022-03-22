2301c21d88dbbd500b1041f4335845b040c017f1
Omar Abuhadid <omrs2002@yahoo.com>
2022-03-22 09:27:00 +0300

    Add redis Cach

--- MVCWizard.Api/Controllers/EmployeeController.cs
+++ MVCWizard.Api/Controllers/EmployeeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using MVCWizard.Api.Application.Contracts;
 using MVCWizard.Data.Models;
-
+using System.Text;
+using System.Text.Json;
 
 namespace MVCWizard.Api.Controllers
 {
@@ -10,18 +12,24 @@ namespace MVCWizard.Api.Controllers
     public class EmployeeController : ControllerBase
     {
         readonly IEmployeeRepository _dbcontext;
-        public EmployeeController(IEmployeeRepository dbcontext)
+        readonly IDistributedCache _distributedCache;
+
+        public EmployeeController(
+            IEmployeeRepository dbcontext,
+            IDistributedCache distributedCache
+            )
         {
             _dbcontext = dbcontext;
+            _distributedCache = distributedCache;
         }
 
-        [HttpGet]
+        [HttpGet("Get")]
         public async Task<IEnumerable<Employee>> Get()
         {
 
             return await _dbcontext.GetAllEmployeesAsync();
 
-         
+
         }
 
         // GET api/<EmployeeController>/5
@@ -29,25 +37,25 @@ namespace MVCWizard.Api.Controllers
         public async Task<ActionResult<Employee?>> Get(int id)
         {
             return await _dbcontext.GetEmployeeByIDAsync(id);
-        
 
-    }
+
+        }
 
         // POST api/<EmployeeController>
-        [HttpPost]
+        [HttpPost("Post")]
         public async Task<ActionResult<int>> Post([FromBody] Employee emp)
         {
             return await _dbcontext.CreateAsync(emp);
-            
+
         }
 
         // PUT api/<EmployeeController>/5
-        [HttpPut]
+        [HttpPut("Put")]
         public async Task<ActionResult<bool>> Put([FromBody] Employee emp)
         {
             if (emp != null)
             {
-               await _dbcontext.UpdateAsync(emp);
+                await _dbcontext.UpdateAsync(emp);
                 return true;
             }
             return false;
@@ -60,5 +68,36 @@ namespace MVCWizard.Api.Controllers
         {
             return await _dbcontext.DeleteAsync(id);
         }
+
+        [HttpGet("GetDateFromRedis")]
+        public async Task<string> GetDate()
+        {
+            var cacheKey = "datenow";
+            string serializeddatenow;
+            DateTime datenow;
+
+            var redis_datenow= await _distributedCache.GetAsync(cacheKey);
+            if (redis_datenow != null)
+            {
+                serializeddatenow = Encoding.UTF8.GetString(redis_datenow);
+                datenow = JsonSerializer.Deserialize<DateTime>(serializeddatenow);
+            }
+            else
+            {
+                datenow = DateTime.Now;
+                serializeddatenow = JsonSerializer.Serialize(datenow);
+                redis_datenow = Encoding.UTF8.GetBytes(serializeddatenow);
+                
+                var options = new DistributedCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(20));
+
+                await _distributedCache.SetAsync(cacheKey, redis_datenow, options);
+            }
+            return datenow.ToString("dd-MM-yyyy HH:mm:ss");
+        }
+
+
+
+
     }
 }
