299f3b55ca72d965e8c7eb9c877c26e53d9fd301
Omar Abuhadid <omrs2002@yahoo.com>
2022-03-24 16:57:11 +0300

    Add Json Helper

---
+++ MVCWizard.Api/Extensions/JsonHelper.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MVCWizard.Api.Extensions
+{
+    public static class JsonHelper
+    {
+        public static T? DynamicDeserialization<T>(string Json, string TokenName) where T : class
+        {
+            dynamic? data = JsonConvert.DeserializeObject(Json);
+            JContainer? Jdata = data;
+            if (Jdata != null & Jdata?.Count > 0)
+            {
+                var SelectedObject = Jdata?.SelectToken(TokenName)?.ToObject<T>();
+                return SelectedObject;
+            }
+
+            return default;
+        }
+
+    }
+}
